2119232c43307c038d6b7ac2b512778a868c6964
Amanda <amanda.souza@mvceditora.com.br>
2024-07-23 10:44:19 -0300

    Adicionando primeiros aprendizados

---
+++ AlgorithmsDataStructures/Program.cs
@@ -0,0 +1,24 @@
+﻿//Arrays
+//Arrays Insertions & Deletion
+
+int[] intArray = new int[6];
+
+int length = 0;
+
+//Add in the end
+
+for (int i = 0; i <  3; i++)
+{
+    intArray[length] = i + 1 ;
+    length++;
+}
+
+//Add in the start
+
+for( int i = 3 ; i >= 0; i--)
+{
+    intArray[i + 1] = intArray[i];
+}
+
+intArray[0] = 20 ;
+length++;
